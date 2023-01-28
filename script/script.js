4217013933e26a79804879767b90e02117f5e3d1
Pedro Lauro <jinnpheonix@protonmail.com>
2023-01-28 20:41:08 -0300

    styling

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 var input_url;
 var btn = document.getElementById("btn");
 var short_url = document.getElementById("shortened-url");
+var short_urls = [];
 
 btn.onclick = function()
 {
@@ -12,6 +13,6 @@ btn.onclick = function()
     .then(api_response => {
         var api_response = JSON.parse(JSON.stringify(api_response));
         console.log(api_response.result.short_link);
-        short_url.innerHTML = api_response.result.short_link;
+        short_url.innerHTML = input_url + "  -  " + api_response.result.short_link;
     });
 };
